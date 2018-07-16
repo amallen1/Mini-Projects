3b7b30f2634b4ec87e45c9f3f0a17f3dd4036c1d
Aniya Allen <amallen046@gmail.com>
2018-07-15 20:28:25 -0500

    Update FibonacciNumberGenerator.java

--- FibonacciNumberGenerator.java
+++ FibonacciNumberGenerator.java
@@ -1,24 +1,4 @@
-//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
-//
-// Title:           (Sequence Generator)
-// Files:           (UTF-8)
-// Course:          (course number, term, and year)
-//
-// Author:          (Aniya Allen)
-// Email:           (aallen27@wisc.edu)
-// Lecturer's Name: (Gary Dahl)
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully 
-// acknowledge and credit those sources of help here.  Instructors and TAs do 
-// not need to be credited here, but tutors, friends, relatives, room mates 
-// strangers, etc do.  If you received no outside help from either type of 
-// source, then please explicitly indicate NONE.
-//
-// Persons:         (identify each person and describe their help in detail)
-// Online Sources:  (identify each URL and describe their assistance in detail)
-//
-/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+
 /**
  * Description of the class comes here
  *
